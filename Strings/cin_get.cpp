9e6289039616d4b4eb1a16a70be390bdea7f9701
Akash <ankitsri66610@gmail.com>
2023-08-27 17:11:48 +0530

    cin get function

---
+++ Strings/cin_get.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+// read a sentence/paragraph and store it
+// cin.get() reads a single character
+// put a loop to read characters till you get newline \n
+
+/*void readline(char a[], int maxLen)
+{
+	
+	int i = 0;
+	char 
+}*/
+
+int main()
+{
+	char ch;
+	ch = cin.get();
+	cout << ch << endl;
+}
