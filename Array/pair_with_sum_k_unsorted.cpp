9fc090bedfb16df0a77adb0be46722493bbb48a2
Akash <ankitsri66610@gmail.com>
2023-08-27 16:33:24 +0530

    Pair with sum k unsorted array

---
+++ Array/pair_with_sum_k_unsorted.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	int a[n];
+	for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	
+	int k;
+	cin >> k;
+	
+	for(int i = 0; i < n - 1; i++)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			if(a[i] + a[j] == k)
+			{
+				cout << a[i] << " + " << a[j] << " = " << k << endl;
+			}
+		}
+	}
+	
+	return 0;
+}
