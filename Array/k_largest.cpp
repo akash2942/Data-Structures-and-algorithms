eaba65e23667d648255aad78f34b3abb0448a827
Akash <ankitsri66610@gmail.com>
2023-09-02 11:05:53 +0530

    k largest elements

---
+++ Array/k_largest.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    sort(arr, arr + n, greater<int>());
+	    
+	    vector<int> ans;
+	    for(int i = 0; i < k; i++)
+	    {
+	        ans.push_back(arr[i]);
+	    }
+	    
+	    return ans;
+	}
+
+};
