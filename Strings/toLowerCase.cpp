b9478b4235dfec2ee7e9605b07863ad4311ef985
Akash <ankitsri66610@gmail.com>
2023-09-25 15:29:56 +0530

    To Lower Case

---
+++ Strings/toLowerCase.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] > 64 && s[i] < 91){
+                s[i] = s[i] + 32;
+            }
+            else{
+                s[i] = s[i];
+            }
+        }
+
+        return s;
+    }
+};
