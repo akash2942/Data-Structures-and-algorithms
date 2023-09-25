d713aafde83d15bb79d5753bca152fb4e225df79
Akash <ankitsri66610@gmail.com>
2023-09-25 15:36:29 +0530

    Find the Difference

---
+++ Strings/difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        int ans[26] = {0};
+
+        for(int i = 0; i < s.length(); i++){
+            ans[s[i] - 'a']++;
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            ans[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(ans[i] != 0){
+                return (char)(i + 'a');
+            }
+        }
+
+        return 'a';
+    }
+};
